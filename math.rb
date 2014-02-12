b41c68330ed5bb3366a4231847dc42765ec6cfd2
isaactullis <irippletv@gmail.com>
2014-02-12 09:29:41 -0600

    Math
    Math for the genius

---
+++ math.rb
@@ -0,0 +1,4 @@
+puts 365 * 24 #hours in a year
+puts 60 * 24 * (3650) # minutes in a decade math = minutes to hours * 3650 
+puts 365 * 24 * 60 * 60 * (19) #my age in seconds would = days * hrs * minutes * seconds * 19
+puts 1025000000 / 60 / 60 / 24 / 365 #authors age math = 1025000000 / seconds / minutes / hours / days = age
